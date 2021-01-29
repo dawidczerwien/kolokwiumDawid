5e47d292d5ca1dbe20a66f4ba23ccbdfff796d9e
Dawid Czerwien <you@example.com>
2021-01-29 16:51:55 +0100

    Gotowe kolokwium

---
+++ kol_1/zad3/CMakeFiles/Student.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Student.dir/src/libtest.cpp.o"
+  "libStudent.a"
+  "libStudent.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Student.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
